36eaf40b126e5a8500206d10d489a2967d817718
Wellington Garcia <garciawell@gmail.com>
2020-01-17 21:02:27 -0300

    Init IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
-import "package:flutter/material.dart";
-import 'package:flutter_todo/pages/home.page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/ui/android/pages/home.page.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Alcool ou Gasolina",
+      title: 'imc',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blue,
       ),
       home: HomePage(),
     );
